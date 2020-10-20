280d35cf9f690cb588e06828cb3c274c964c8209
Josiel Souza <josielsouza.dj@gmail.com>
2020-10-20 13:47:51 -0300

    criar página login

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php require("includes/header.php");?>
+
+
+<main>
+        <div class="gradiente">
+
+            <div class="main-logo">
+                <img src="logo.png" alt="">
+            </div>
+
+
+        </div>
+    </main>
+
+
+<?php require("includes/footer.php");?>
