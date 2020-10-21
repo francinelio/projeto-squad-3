b8211a29e360538d74cd1f93a6ceb345876d33e1
Josiel Souza <josielsouza.dj@gmail.com>
2020-10-21 12:44:50 -0300

    dar forma ao formulário

--- login.php
+++ login.php
@@ -4,15 +4,16 @@
 <main>
     <div class="gradiente">
         <div class="area_login">
-
-            <div class="main_logo">
-                <img src="projeto-squad-3/assets/img/logo.png" alt="">
+            <div class="text-center">
+                <img src="assets/img/logo.png" alt="">
             </div>
 
-            <form>
+
+
+            <form class="bg-white p-5 m-5">
                 <div class="col-auto">
                     <label class="sr-only" for="inlineFormInputGroup">Username</label>
-                    <div class="input-group mb-2">
+                    <div class="input-group my-2">
                         <div class="input-group-prepend">
                             <div class="input-group-text">@</div>
                         </div>
