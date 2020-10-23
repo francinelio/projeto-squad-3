012c7439d51449913afbb16da6db6314b5ef598e
Josiel Souza <josielsouza.dj@gmail.com>
2020-10-22 22:56:54 -0300

    ajustar form

--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
+    <!--font dos ícones -->
 
     <title>Hello, world!</title>
 </head>
--- login.php
+++ login.php
@@ -3,24 +3,51 @@
 
 <main>
     <div class="gradiente">
-        <div class="area_login">
-            <div class="text-center">
+        <div class="text-center p-5">
+            <div class="text-center mb-5">
                 <img src="assets/img/logo.png" alt="">
             </div>
-
-
+            <!--fim da div logo-->
 
             <form class="bg-white p-5 m-5">
                 <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInputGroup">Username</label>
                     <div class="input-group my-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">@</div>
+                        <div class="input-group-prepend ">
+                            <div class="input-group-text "> <i class="far fa-user"></i> </div>
                         </div>
-                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Nome Completo">
                     </div>
                 </div>
+
+                <div class="col-auto">
+                    <div class="input-group my-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"> <i class="far fa-envelope"></i> </div>
+                        </div>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+                    </div>
+                </div>
+
+                <div class="col-auto">
+                    <div class="input-group my-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fas fa-eye"></i></div>
+                        </div>
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Senha">
+                    </div>
+                </div>
+
+                <div class="text-center p-3">
+                    <div>
+                        <button type="submit" class="btn btn-primary mb-3 badge badge-pill ">Entrar</button>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-info mb-4  badge badge-pill ">Cadastrar</button>
+                    </div>
+                </div>
+
             </form>
+            <!--fim do formulário-->
 
         </div>
     </div>
